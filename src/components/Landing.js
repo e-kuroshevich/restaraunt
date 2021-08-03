69cd173f5222494d29e2c60dc8850645c40d0699
Evgeny Kuroshevich <e.kuroshevich@gmail.com>
2021-08-03 10:22:37 +0300

    React - Lesson 6 add events and this

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,11 +2,23 @@ import React from "react";
 import restaurants from "../sample-restaurants";
 
 class Landing extends React.Component {
+  state = {
+    display: false,
+    title: "",
+    url: "",
+  };
+  displayList = () => {
+    console.log(this);
+  };
   render() {
     return (
       <div className='restaurant_select'>
         <div className='restaurant_select_top'>
-          <div className='restaurant_select_top-header'>Select restaurant</div>
+          <div
+            onClick={this.displayList}
+            className='restaurant_select_top-header'>
+            Select restaurant
+          </div>
           <div className='arrow_picker'>
             <div className='arrow_picker-up'></div>
             <div className='arrow_picker-down'></div>
@@ -15,7 +27,7 @@ class Landing extends React.Component {
         <div className='restaurant_select_bottom'>
           <ul>
             {restaurants.map((restaurant) => {
-              return <li key={restaurant.id}>{restaurant.title}</li>
+              return <li key={restaurant.id}>{restaurant.title}</li>;
             })}
           </ul>
         </div>
