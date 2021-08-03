60b8985beb087e654eafb41a64a6eb3989e995a3
Evgeny Kuroshevich <e.kuroshevich@gmail.com>
2021-08-03 08:37:43 +0300

    React - Lesson 4

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import restaurants from "../sample-restaurants";
 
 class Landing extends React.Component {
   render() {
-    return(
-      <p>Hello App!!</p>
-    )
+    return (
+      <div className='restaurant_select'>
+        <div className='restaurant_select_top'>
+          <div className='restaurant_select_top-header'>Select restaurant</div>
+          <div className='arrow_picker'>
+            <div className='arrow_picker-up'></div>
+            <div className='arrow_picker-down'></div>
+          </div>
+        </div>
+        <div className='restaurant_select_bottom'>
+          <ul>
+            {restaurants.map((restaurant) => {
+              return <li key={restaurant.id}>{restaurant.title}</li>
+            })}
+          </ul>
+        </div>
+        <button>Go To restaurant</button>
+      </div>
+    );
   }
 }
 export default Landing;
---
+++ src/sample-restaurants.js
@@ -0,0 +1,18 @@
+const restaurants = [
+  {
+    title: "Hot Burger Tverskaya",
+    url: "hot-burger-tverskaya",
+    id: 1,
+  },
+  {
+    title: "Hot Burger Maroseika",
+    url: "hot-burger-maroseika",
+    id: 2,
+  },
+  {
+    title: "Hot Burger Petrovka",
+    url: "hot-burger-petrovka",
+    id: 3,
+  },
+];
+export default restaurants;
